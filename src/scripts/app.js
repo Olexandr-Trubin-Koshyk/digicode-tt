7da82c9142d3c72c3e35e9c6e4d4d8920426704e
Alexandr Trubin-Koshyk <alexandr.koshyk@gmail.com>
2022-06-12 19:11:53 +0300

    Added main js file + added ts

---
+++ src/scripts/app.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var pixi_js_1 = require("pixi.js");
+var app = new pixi_js_1.Application({
+    width: 800,
+    height: 600,
+    backgroundColor: 0xc2c2c2,
+    view: document.getElementById("canvas")
+});
